c0df5fb0ba28e96cfc100ad296ba76d39bc32b48
Luis Diaz <luisgdiazreynoso@gmail.com>
2020-10-18 22:33:41 -0700

    :hankey: Made 2 navbars for header. Bad coding.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { pulseAnimation } from 'angular-animations';
 import { trigger, state, style, animate, transition, keyframes, query } from '@angular/animations';
 import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
@@ -8,7 +7,6 @@ import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-s
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
   animations: [
-    pulseAnimation(),
     trigger('openCloseMenu', [
       state('open', style({
         height: '*',
@@ -20,21 +18,27 @@ import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-s
       })),
       transition('open => closed', [
         query('ul', style({ height: '*' })),
-        query('ul', animate('500ms', style({ height: '0px' }))),
+        query('ul', animate('500ms', keyframes([
+          style({ height: '*',  offset: 0 }),
+          style({ height: '0px',  offset: 0.25 }),
+        ]))),
         animate('500ms', keyframes([
-          style({ opacity: 1,  offset: 0 }),
-          style({ opacity: 0, offset: 0.25 }),
-          style({ height: '*',  offset: 0.26 }),
-          style({ height: '0px',  offset: 1 }),
+          style({ height: '*',  offset: 0 }),
+          style({ height: '0px',  offset: 0.25 }),
+          style({ opacity: 1,  offset: 0.75 }),
+          style({ opacity: 0, offset: 1 }),
         ]))
       ]),
       transition('closed => open', [
         query('ul', style({ height: '0px' })),
-        query('ul', animate('500ms', style({ height: '*' }))),
+        query('ul', animate('500ms', keyframes([
+          style({ height: '0px',  offset: 0 }),
+          style({ height: '*',  offset: 0.25 }),
+        ]))),
         animate('500ms', keyframes([
           style({ height: '0px',  offset: 0 }),
-          style({ height: '*',  offset: 0.75 }),
-          style({ opacity: 0,  offset: 0.76 }),
+          style({ height: '*',  offset: 0.25 }),
+          style({ opacity: 0,  offset: 0.26 }),
           style({ opacity: 1, offset: 1 })
         ]))
       ])
@@ -47,11 +51,9 @@ export class SidebarComponent implements OnInit {
   faInstagram = faInstagram;
   faWhatsapp = faWhatsapp;
 
-  hoverBrand: boolean;
   menu: boolean;
 
   constructor() {
-    this.hoverBrand = false;
     this.menu = false;
   }
 
