f7dab9b60e189739bc4848f1c8bf55f97154b968
Luis Diaz <luisgdiazreynoso@gmail.com>
2021-03-05 15:39:25 -0800

    :hammer: Fixing system environment for heroku

--- set-env.ts
+++ set-env.ts
@@ -3,13 +3,20 @@ const fs = require('fs');
 const writeFile = fs.writeFile;
 // Configure Angular `environment.ts` file path
 const targetPath = './src/environments/environment.ts';
+const targetPathProd = './src/environments/environment.prod.ts';
 // Load node modules
 const colors = require('colors');
 require('dotenv').config();
 
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
-    production: ${process.env.production},
+    production: false,
+    gmapsKey: '${process.env.gmapsKey}',
+};
+`;
+// `environment.prod.ts` file structure
+const envConfigFileProd = `export const environment = {
+    production: true,
     gmapsKey: '${process.env.gmapsKey}',
 };
 `;
@@ -23,3 +30,10 @@ writeFile(targetPath, envConfigFile, function (err) {
        console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
    }
 });
+writeFile(targetPathProd, envConfigFileProd, function (err) {
+    if (err) {
+        throw console.error(err);
+    } else {
+        console.log(colors.magenta(`Angular environment.prod.ts file generated correctly at ${targetPathProd} \n`));
+    }
+ });
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    gmapsKey: 'AIzaSyBIzpYyzXIQYiaVstaBg34wQ2X0ZLz9jiE',
-};
